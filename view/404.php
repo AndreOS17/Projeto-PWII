5bc2eed8801247a6a98cb92f37a9b9c7498cee1f
AndreOS17 <andre.os908@gmail.com>
2020-07-13 19:14:27 +0000

    Atualização da Página 404

--- view/404.php
+++ view/404.php
@@ -1,12 +1,23 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ERRO!</title>
-</head>
-<body>
-    <h1>Página não Encontrada!</h1>
-    <a href="galeria.php">Voltar para Página Inicial</a>
-</body>
+<?php include "head.php" ?>
+
+    <body>
+        <!-- Links:  -->
+        <nav class="nav-extended brown darken-3">
+            <div class="nav-wrapper n">
+                <ul id="nav-mobile" class="right"></ul>
+            </div>
+            <!-- Logo:  -->
+            <div class="nav-header center">
+                <h1 class="tituloSite">CLOROGAMES</h1>
+            </div>
+            <!-- Navtabs:  -->
+            <div class="nav-content">
+                <ul class="tabs tabs-transparent grey darken-4 n"></ul>
+            </div>
+        </nav>
+
+        <h1 class="center" id="Erro">Página não Encontrada!</h1>
+        <center><img class="img404" height="150dp" src="./imagens/Error-404Window-icon.png" alt="Erro!"></center>
+        <center><a class="btn404" href="/">Voltar para o Início</a></center>
+        
 </html>
